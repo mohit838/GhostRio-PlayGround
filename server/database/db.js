a8689ecc3dba391e6617c6bf3654f7ae4dcd420f
mohit838 <mohitul449@gmail.com>
2023-07-09 23:07:55 +0600

    update db connections

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
 
 async function connect() {
-  mongoose.set('strictQuery', true);
-  const db = await mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log('Database Connected Successfully!!');
-  return db;
+  try {
+    mongoose.set('strictQuery', true);
+    const db = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database Connected Successfully!!');
+    return db;
+  } catch (error) {
+    console.log(error.message);
+    process.exit;
+  }
 }
 
 export default connect;
