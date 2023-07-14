e327f84cd91fb1c3f98a2d0c2b102723a197e89d
mohit838 <mohitul449@gmail.com>
2023-07-14 16:51:41 +0600

    from in home

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./componetnts/Home";
 
-export default App;
+/** root routes */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
+
+export default function App() {
+  return (
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
+}
---
+++ client/src/componetnts/Regiser.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Regiser = () => {
+  return <div>Regiser</div>;
+};
+
+export default Regiser;
