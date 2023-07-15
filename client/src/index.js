d5e135c598b05660de2c03fa8d2645873de29ace
mohit838 <mohitul449@gmail.com>
2023-07-15 10:11:43 +0600

    Added To WebStrom

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import './middleware/axios'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </React.StrictMode>
 );
--- client/src/middleware/axios.js
+++ client/src/middleware/axios.js
@@ -3,36 +3,36 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:7000/api/";
 
 axios.interceptors.response.use(
-  (resp) => resp,
-  async (err) => {
-    if (err.response.status === 401 || err.response.status === 403) {
-      const refreshToken = sessionStorage.getItem("refreshToken");
+    (resp) => resp,
+    async (err) => {
+        if (err.response.status === 401 || err.response.status === 403) {
+            const refreshToken = sessionStorage.getItem("refreshToken");
 
-      if (refreshToken) {
-        const response = await axios.post(
-          "refresh-token",
-          { refreshToken },
-          { withCredentials: true }
-        );
+            if (refreshToken) {
+                const response = await axios.post(
+                    "refresh-token",
+                    {refreshToken},
+                    {withCredentials: true}
+                );
 
-        if (response.status === 200) {
-          const { accessToken } = response.data;
+                if (response.status === 200) {
+                    const {accessToken} = response.data;
 
-          axios.defaults.headers.common[
-            "Authorization"
-          ] = `Bearer ${accessToken}`;
+                    axios.defaults.headers.common[
+                        "Authorization"
+                        ] = `Bearer ${accessToken}`;
 
-          // Update the refreshed access token in session storage
-          sessionStorage.setItem("refreshToken", accessToken);
+                    // Update the refreshed access token in session storage
+                    sessionStorage.setItem("refreshToken", accessToken);
 
-          // Retry the failed request with the new access token
-          err.config.headers["Authorization"] = `Bearer ${accessToken}`;
+                    // Retry the failed request with the new access token
+                    err.config.headers["Authorization"] = `Bearer ${accessToken}`;
 
-          return axios(err.config);
+                    return axios(err.config);
+                }
+            }
         }
-      }
-    }
 
-    return Promise.reject(err);
-  }
+        return Promise.reject(err);
+    }
 );
