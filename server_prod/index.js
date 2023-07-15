4df0d9d4e7821d3aaca96cc2b128c68ee3105ba7
mohit838 <mohitul449@gmail.com>
2023-07-15 14:00:15 +0600

    udpate with latest

--- server_prod/index.js
+++ server_prod/index.js
@@ -1,9 +1,9 @@
-import express from 'express'
 import bodyParser from "body-parser";
-import cors from 'cors'
-import {readdirSync} from "fs";
+import cors from "cors";
+import express from "express";
+import { readdirSync } from "fs";
+import { PORT } from "./config/config.js";
 import connect from "./db/db.js";
-import {PORT} from "./config/config.js";
 
 // Base Configurations
 const app = express();
@@ -12,32 +12,30 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 // Cors Policies
-
-// This is use for all access type
-// app.use(cors())
-
 // Restrictions in accessing
-app.use(cors({
+app.use(
+  cors({
     // If you want to allow requests from any origin, you can set origin: '*'.
-    origin: 'http://localhost:3000', // If you want any restrictions then set frontend url
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+    origin: "http://localhost:3000", // If you want any restrictions then set frontend url
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // All Routers of this projects
 const routes = readdirSync("./routes");
 routes.forEach(async (route) => {
-    const routeModule = await import(`./routes/${route}`);
-    app.use("/api", routeModule.default);
+  const routeModule = await import(`./routes/${route}`);
+  app.use("/api", routeModule.default);
 });
 
 // MongoDB and Server Setups
-const SERVER_PORT = PORT || 7000;
+const SERVER_PORT = PORT || 8000;
 
 // Connect DB first then server started
 connect().then(() => {
-    app.listen(SERVER_PORT, () =>
-        console.log(`Nymphelia Server Running On Port: ${SERVER_PORT}`)
-    );
+  app.listen(SERVER_PORT, () =>
+    console.log(`Nymphelia Server Running On Port: ${SERVER_PORT}`)
+  );
 });
